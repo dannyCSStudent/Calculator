3d7efbc605b6df5c1889494889b292f967d7e481
Dan Villegas <danv8605@gmail.com>
2022-11-05 15:38:49 -0600

    Added js to make calculator work
    Storing the first number to calculate in firstValue
    variable. Then after the math symbol is clicked,
    the symbol will also be stored in symbol variable.
    Then, the second number will be stored in
    secondValue variable. Finally after the equal button
    is clicked, the operate function will be called and
    passed three parameters: firstValue, secondValue, and
    symbol.

--- js-file.js
+++ js-file.js
@@ -1,20 +1,113 @@
+let firstValue = "";
+let isFirstValue = true;
+let secondValue = "";
+let isSecondVaue = false;
+let symbol = "";
+let result = 0;
+//#region 
 const add = function(a, b) {
     return a + b;
 };
 
 const substract = function(a, b) {
     return a - b;
-}
+};
 
 const multiple = function(a, b) {
     return a * b;
-}
+};
 
 const divide = function(a, b) {
     if (b == 0) {
         return "Cannot divide by 0";
-    }
+    };
     return a / b;
-}
-console.log(divide(10, 0));
+};
+//#endregion
+
+const operate = function(a, b, operator) {
+    
+    switch (operator) {
+        case "+":
+            
+            return add(a, b);
+        case "-":
+            return substract(a, b);
+        case "x":
+            return multiple(a, b);
+        case "/":
+            return divide(a, b);
+    };
+};
+
+const buttons = document.querySelectorAll(".digit");
+const signs = document.querySelectorAll(".sign");
+const input = document.getElementById("digits");
+const calculate = document.getElementById("calculation");
+const equalSign = document.getElementById("result");
+
+equalSign.onclick = () => {
+    if (isFirstValue === false) {
+        calculate.textContent = firstValue + 
+            " " + symbol + " " + secondValue;
+        input.textContent = "";
+        input.textContent = operate(getFirstValue(),
+            getSecondValue(), symbol);
+    };
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (isFirstValue === true) {
+            displayFirstValue(button.value);
+        }
+        else {
+            displaySecondValue(button.value);
+        }
+    });
+});
+
+signs.forEach((sign) => {
+    sign.addEventListener("click", () => {
+        if (!(isFirstValue)) {
+            return;
+        };
+        symbol = sign.value;
+        calculate.textContent = getFirstValue() + " " + symbol;
+        input.textContent = "";
+        input.textContent = "0";
+        isFirstValue = false;
+    
+    });
+});
+
+
+function displayFirstValue(a) {
+    
+    firstValue += a;
+    if (firstValue.length > 14) {
+        return;
+    };
+
+    input.textContent = firstValue;
+};
+
+function displaySecondValue(a) {
+    
+    secondValue += a;
+    if (secondValue.length > 14) {
+        return;
+    };
+
+    input.textContent = secondValue;
+};
+
+function getFirstValue() {
+    return parseFloat(firstValue);
+};
+function getSecondValue() {
+    return parseFloat(secondValue);
+};
+
+
 
